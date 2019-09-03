f720839c709ad0b9d14ff497bf51b5f6c170c2c2
Elmeri1 <47521656+Elmeri1@users.noreply.github.com>
2019-09-03 13:58:11 +0300

    Add files via upload

--- slotgames/index.js
+++ slotgames/index.js
@@ -1,43 +1,84 @@
 
-var rahat = 50;
+var n1, n2, n3;
+var rahat = 100;
+// var slot3 = 0;
+// var slot2 = 0;
+// var slot1 = 0;
+var n1 = 0;
+var n2 = 0;
+var n3 = 0;
+var lukko1 = 0;
+var lukko2 = 0;
+var lukko3 = 0;
+
+var lukitseminen = false;
 
 var info = document.getElementById('info');
 
 
-function slot1() {
+function slot() {
     return Math.floor(Math.random() * 3);
 }
 
-function slot2() {
-    return Math.floor(Math.random() * 3);
-}
-
-function slot3() {
-   return Math.floor(Math.random() * 3);
-    //document.getElementById('slot3').innerHTML = slot3();
-}
-
-function voitto() {
+function voitto(slot1, slot2, slot3) {
     if (slot1 == slot2 && slot2 == slot3) {
         rahat = rahat + 5;
     } else {
         rahat = rahat -1;
     }
-    
+    return rahat;
 }
 
 function kierros() {
 
-    // var n1 = slot1();
-    // var n2 = slot1();
+    if (lukko1 == 0) {
+        n1 = slot();
+    } else {
+        lukko1 = 0;
+    }
 
-    document.getElementById("slot1").innerHTML = slot1();
-    document.getElementById("slot2").innerHTML = slot2();
-    document.getElementById("slot3").innerHTML = slot3();
-    document.getElementById("raha").innerHTML = voitto();
-    // slot1();
-    // slot2();
-    // slot3();
+    if (lukko2 == 0) {
+        n2 = slot();
+    } else {
+        lukko2 = 0;
+    }
+
+    if (lukko3 == 0) {
+        n3 = slot();
+    } else {
+        lukko3 = 0;
+    }
+
+    if (rahat < 1) {
+        return;
+    }
+    document.getElementById("slot1").innerHTML = n1;
+    document.getElementById("slot2").innerHTML = n2;
+    document.getElementById("slot3").innerHTML = n3;
+    document.getElementById("raha").innerHTML = voitto(n1, n2, n3);
+
 }
 
+function lukitse(s) {
 
+    // muutetaan valittu lukko-muuttuja ykköseksi
+    if (s == 1){
+        lukko1 = 1;
+    }
+    if (s == 2) {
+        lukko2 = 1;
+    }
+    if (s == 1){
+        lukko3 = 1;
+    }
+
+    update();
+
+
+
+
+}
+
+function update() {
+
+}
