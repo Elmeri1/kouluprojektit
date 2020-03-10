ac1224fa0a981325a6adfd2b1a461d4dad74d3f6
Elmeri1 <47521656+Elmeri1@users.noreply.github.com>
2020-03-10 12:34:43 +0200

    Add files via upload

---
+++ Viestiseina/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/PHP/Viestiseina');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'viestiseina');
